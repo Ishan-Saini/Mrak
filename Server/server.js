711657d23e547e73e22eed4caa3948bc73838930
Ishan-Saini <ishansaini243@gmail.com>
2021-10-18 18:32:09 +0530

    Setup server &  MVC structure

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
