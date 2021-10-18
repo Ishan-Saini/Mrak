784365e6e5cf50d9d3f472208f283db4339e1fe0
Ishan-Saini <ishansaini243@gmail.com>
2021-10-18 19:00:20 +0530

    Setup mongoDB connection

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const db = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database connected successfully');
+  });
 
 const port = process.env.PORT || 5000;
 
