e847589589a7f657fd319789ac03f0133da3fed2
Ishan-Saini <ishansaini243@gmail.com>
2021-10-21 10:55:08 +0530

    setup user route

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/users', userRoutes);
 
 module.exports = app;
