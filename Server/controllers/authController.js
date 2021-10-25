2c59f62c47a386832654b9f08ec5aee41a47f271
Ishan-Saini <ishansaini243@gmail.com>
2021-10-25 23:05:36 +0530

    Add generateJwt function

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,1 +1,22 @@
-exports.signup = () => {};
+const jwt = require('jsonwebtoken');
+
+const asyncUtility = require('../util/asyncUtility');
+const User = require('../models/userModel');
+
+const generateJwt = async (user, res, statusCode) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_PRIVATE_KEY, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = asyncUtility(async (req, res, next) => {});
