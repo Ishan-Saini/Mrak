d32cd22cab4050453366fc179247a80416d6bfb1
Ishan-Saini <ishansaini243@gmail.com>
2021-11-19 22:30:12 +0530

    Add signup in authController

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -19,4 +19,13 @@ const generateJwt = async (user, res, statusCode) => {
   });
 };
 
-exports.signup = asyncUtility(async (req, res, next) => {});
+exports.signup = asyncUtility(async (req, res, next) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  generateJwt(user, res, 201);
+});
