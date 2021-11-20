d3467b61b9efa3418e78d871c39bee35db7879c8
Ishan-Saini <ishansaini243@gmail.com>
2021-11-20 10:35:18 +0530

    Add production error logic

--- Server/controllers/errorController.js
+++ Server/controllers/errorController.js
@@ -1,5 +1,4 @@
 //Send Development Error
-
 const devErrorHandler = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -10,12 +9,25 @@ const devErrorHandler = (err, res) => {
 };
 
 // Send Production Error
-
 const prodErrorHandler = (err, res) => {
-  console.log(err);
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went wrong! Try again later',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
-  if (process.env.NODE_ENV === 'production') prodErrorHandler(err, res);
-  else devErrorHandler(err, res);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'production') {
+    prodErrorHandler(err, res);
+  } else devErrorHandler(err, res);
 };
